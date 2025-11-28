ee976e2566f2baf981cde98112c011c70f505bbd
garvitKhatod6 <khatodg2006@gmail.com>
2025-11-29 00:12:24 +0530

    Create Q135.c

---
+++ Q135.c
@@ -0,0 +1,14 @@
+/*Q135: Assign explicit values starting from 10 and print them.*/
+
+
+#include <stdio.h>
+enum values{
+    a=10,b,c
+};
+int main(){
+printf("A:%d\n",a);
+printf("B:%d\n",b);
+printf("C:%d\n",c);
+
+    return 0;
+}
